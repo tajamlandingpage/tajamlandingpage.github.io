22be49958706b6752fd4bad6241b57f9ca1d6f19
tajamlandingpage <56261744+tajamlandingpage@users.noreply.github.com>
2019-11-30 20:23:31 +0300

    Add files via upload

--- src/js/main.js
+++ src/js/main.js
@@ -1,44 +1,21 @@
 $(document).ready(function () {
-  //слайдер с работами
-  $('.autoplay').slick({
-    slidesToShow: 5,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    responsive: [{
-        breakpoint: 1100,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 1,
-          infinite: true
-        }
-      },
-      {
-        breakpoint: 700,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-    ]
-  });
 
-  //слайдер с отзывами
-  $('.single-item').slick({
-    fade: true,
-    autoplay: true,
-    infinite: true,
-    speed: 500,
-    prevArrow: '<button type="button" class="prev"></button>',
-    nextArrow: '<button type="button" class="next"></button>',
-    responsive: [
-    {
-      breakpoint: 700,
-      settings: {
-        arrows: false
-      }
-    }
-  ]
-  });
+    $('.single-item').slick({
+        fade: true,
+        autoplay: true,
+        infinite: true,
+        speed: 500,
+        prevArrow: '<button type="button" class="prev"></button>',
+        nextArrow: '<button type="button" class="next"></button>',
+    });
+
+
+    $('a[href*="#"]').on('click', function (e) {
+        e.preventDefault();
+
+        $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top
+        }, 700, 'linear');
+    });
 
 });
